9b82eb707fe11fa57545dfc80afc2af3a6c1dec1
Qing ’s <haoqing3059@icloud.com>
2019-01-30 15:18:27 +0800

    MVVM

--- Sky/Sky/ViewControllers/LocationsViewController/AddLocationViewController.swift
+++ Sky/Sky/ViewControllers/LocationsViewController/AddLocationViewController.swift
@@ -15,97 +15,75 @@ protocol AddLocationViewControllerDelegate {
 
 class AddLocationViewController: UITableViewController {
     @IBOutlet weak var searchBar: UISearchBar!
-    
-    private var locations: [Location] = []
-    
-    private lazy var geocoder = CLGeocoder()
-    
+    var viewModel: AddLocationViewModel!
     var delegate: AddLocationViewControllerDelegate?
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Add a location"
+        viewModel = AddLocationViewModel()
+        
+        viewModel.locationsDidChange = {
+            [unowned self] locations in
+            self.tableView.reloadData()
+        }
+        
+        viewModel.queryingStatusDidChange = {
+            [unowned self] isQuerying in
+            if isQuerying {
+                self.title = "Searching..."
+            } else {
+                self.title = "Add a location"
+            }
+        }
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         // Show Keyboard
         searchBar.becomeFirstResponder()
     }
-    
-    private func geocode(address: String?) {
-        guard let address = address else {
-            locations = []
-            tableView.reloadData()
-            
-            return
-        }
-        
-        geocoder.geocodeAddressString(address) {
-            [weak self] (placemarks, error) in
-            DispatchQueue.main.async {
-                self?.processResponse(with: placemarks, error: error)
-            }
-        }
-    }
-    
-    private func processResponse(with placemarks: [CLPlacemark]?, error: Error?) {
-        if let error = error {
-            print("Cannot handle Geocode Address! \(error)")
-        } else if let results = placemarks {
-            locations = results.compactMap { result -> Location? in
-                guard let name = result.name else { return nil }
-                guard let location = result.location else { return nil }
-                
-                return Location(name: name,latitude: location.coordinate.latitude,longitude: location.coordinate.longitude)
-            }
-            tableView.reloadData()
-        }
-    }
 }
 
 extension AddLocationViewController {
-
+    
+    // MARK: - Table view data source
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return locations.count
+        return viewModel.numberOfLocations
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(
-            withIdentifier: LocationTableViewCell.reuseIdentifier, for: indexPath)
-            as? LocationTableViewCell else { fatalError("Unexpected table view cell") }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: LocationTableViewCell.reuseIdentifier, for: indexPath) as? LocationTableViewCell else { fatalError("Unexpected table view cell") }
         
-        let location = locations[indexPath.row]
-        let vm = LocationsViewModel(location: location.location, locationText: location.name)
-        
-        cell.configure(with: vm)
+        if let viewModel = viewModel.locationViewModel(at: indexPath.row) {
+            cell.configure(with: viewModel)
+        }
         
         return cell
     }
-
+    
     // MARK: - Table view delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let location = locations[indexPath.row]
+        guard let location = viewModel.location(at: indexPath.row) else { return }
         
         delegate?.controller(self, didAddLocation: location)
         
         navigationController?.popViewController(animated: true)
     }
-
+    
 }
 
 extension AddLocationViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
-        geocode(address: searchBar.text)
+        viewModel.queryText = searchBar.text ?? ""
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
-        locations = []
-        tableView.reloadData()
+        viewModel.queryText = searchBar.text ?? ""
     }
 }
